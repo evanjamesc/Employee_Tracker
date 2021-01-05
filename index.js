938b64430a16e6a53ffdaa6af55e3e6a0ffc461e
Evan Christianson <evanjameschristianson@gmail.com>
2021-01-04 20:34:06 -0600

    viewEmployees added connection query

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
 // This line was added by VS Code when I created 'connection', not sure what it is:
-//const { connect } = require('http2');
+// const { connect } = require('http2');
 
 let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Radar9518',
-    database: 'employees_db'
+    password: 'Radar9518!',
+    database: 'employee_db'
 });
 
-connection.connect(function (err) {
+connection.connect(function(err) {
     if (err) {
         console.error('Cannot connect to database: ' + err.stack);
         return;
     }
 
-    console.log('Connected. ID: ');
+    console.log('Connected');
 });
 
 let menu = async () => {
@@ -80,7 +80,7 @@ let menu = async () => {
 }
 
 let viewEmployees = () => {
-
+    connection.query();
 }
 
 let viewDepts = () => {
