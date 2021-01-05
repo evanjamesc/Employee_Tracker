78c110c2dd59c1a0434e27c3868a7d570c629939
Evan Christianson <evanjameschristianson@gmail.com>
2021-01-04 19:29:10 -0600

    Connection error handling

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
+// This line was added by VS Code when I created 'connection', not sure what it is:
+//const { connect } = require('http2');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Radar9518',
+    database: 'employees_db'
+});
+
+connection.connect(function(err)
+{
+    if (err) {
+        console.error('Cannot connect to database: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected. ID: ');
+});
